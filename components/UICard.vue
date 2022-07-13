e2e2e717673129395a4c58e7c3f55913d9e6278c
divnvpp <pimjennifer@gmail.com>
2022-07-13 14:52:31 +0900

    add: UICard.vue

---
+++ components/UICard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <slot name="content" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard"
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 4px;
+  background-color: #FFFEFB;
+  box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+}
+</style>
