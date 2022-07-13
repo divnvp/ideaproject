79a8d91a1a7205e1bfb4ada251bc5c7ae34e533d
divnvpp <pimjennifer@gmail.com>
2022-07-13 16:14:11 +0900

    add: UISelector.vue file

---
+++ components/UISelector.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <select class="select">
+      <option selected>По умолчанию</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UISelector",
+
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+.select {
+  border: none;
+
+  padding: 10px;
+
+  background: #FFFEFB;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+
+  color: #B4B4B4;
+}
+</style>
