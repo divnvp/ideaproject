a2814bb637051add2f8de96904c74dc808c4869d
Diana <divnvp@yandex.com>
2022-07-13 20:23:05 +0900

    add: UIInput.vue

---
+++ components/UIInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="custom-input">
+    <span class="custom-input__span">{{ title }}</span>
+    <input
+      class="custom-input__input"
+      :placeholder="placeholder"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UIInput",
+
+  props: {
+    title: { type: String },
+    placeholder: { type: String },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.custom-input {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+
+  &__span {
+    font-size: 10px;
+    font-weight: 400;
+    color: #49485E;
+  }
+
+  &__input {
+    margin-top: 4px;
+    padding: 16px;
+
+    border: none;
+    background: #FFFEFB;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+
+    &::placeholder {
+      font-size: 12px;
+      color: #B4B4B4;
+    }
+  }
+}
+</style>
