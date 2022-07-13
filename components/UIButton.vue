3b0318152acb8e16de1f6f627310c004f34ea347
divnvpp <pimjennifer@gmail.com>
2022-07-13 14:52:22 +0900

    add: UIButton.vue

---
+++ components/UIButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button class="button" :disabled="disabled">
+      {{ title }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UIButton",
+
+  props: {
+    title: { type: String, required: true },
+    disabled: { type: Boolean, required: true },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: 'Inter', serif;
+  font-weight: 600;
+  font-size: 12px;
+
+  border: none;
+  border-radius: 10px;
+
+  width: 100%;
+  padding: 10px;
+
+  &:disabled {
+    background-color: #EEEEEE;
+    color: #B4B4B4;
+  }
+}
+</style>
