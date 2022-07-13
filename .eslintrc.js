38ba1a4e3835884673f3a05926e54b2817af0851
Diana <divnvp@yandex.com>
2022-07-13 20:22:35 +0900

    add: ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential"
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      4
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "max-len": [
+      "error", {
+        "code": 80,
+        "template": 80,
+        "tabWidth": 2,
+        "comments": 80,
+      }
+    ]
+  }
+};
