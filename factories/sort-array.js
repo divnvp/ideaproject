0da1d7617f963bcfe26aeb61c3154f07be56e0ce
Diana <divnvp@yandex.com>
2022-07-15 12:27:40 +0900

    add: factory for sort array

---
+++ factories/sort-array.js
@@ -0,0 +1,26 @@
+export function sortArray(value, array) {
+  switch (value) {
+    case "min":
+      array = array.sort((a, b) =>
+        Number(a.price.replaceAll(" ", "")) -
+        Number(b.price.replaceAll(" ", ""))
+      );
+      break;
+    case "max":
+      array = array.sort((a, b) =>
+        Number(b.price.replaceAll(" ", "")) -
+        Number(a.price.replaceAll(" ", ""))
+      );
+      break;
+    case "name":
+      array = array.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+      break;
+    case "default":
+    default:
+      array = JSON.parse(localStorage.getItem("products"));
+  }
+
+  return array;
+}
