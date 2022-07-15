346f658218d8b408dacdb1c42ea95b75ed198103
Diana <divnvp@yandex.com>
2022-07-15 18:32:18 +0900

    update: with linter

--- factories/sort-array.js
+++ factories/sort-array.js
@@ -1,22 +1,22 @@
 export function sortArray(value, array) {
   switch (value) {
-    case "min":
-      array = array.sort((a, b) =>
-        Number(a.price.replaceAll(" ", "")) -
+  case "min":
+    array = array.sort((a, b) =>
+      Number(a.price.replaceAll(" ", "")) -
         Number(b.price.replaceAll(" ", ""))
-      );
-      break;
-    case "max":
-      array = array.sort((a, b) =>
-        Number(b.price.replaceAll(" ", "")) -
+    );
+    break;
+  case "max":
+    array = array.sort((a, b) =>
+      Number(b.price.replaceAll(" ", "")) -
         Number(a.price.replaceAll(" ", ""))
-      );
-      break;
-    case "name":
-      array = array.sort((a, b) =>
-        a.name.localeCompare(b.name)
-      );
-      break;
+    );
+    break;
+  case "name":
+    array = array.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    break;
   }
 
   return array;
